5e1346d759ffa09aaec71bb7e0365aa1772fa34f
Simon Perrigault <simonperrigault22@gmail.com>
2025-03-11 15:52:28 +0100

    unary minus and not

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -3,8 +3,8 @@
 antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx) 
 {
     std::cout << ".text\n";
-    std::cout<< ".globl main\n" ;
-    std::cout<< "main: \n" ;
+    std::cout<< ".globl main\n";
+    std::cout<< "main: \n";
 
     std::cout << "    pushq %rbp # save %rbp on the stack\n";
     std::cout << "    movq %rsp, %rbp # define %rbp for the current function\n";
@@ -19,7 +19,7 @@ antlrcpp::Any CodeGenVisitor::visitDecl_stmt(ifccParser::Decl_stmtContext *ctx)
     if (ctx->expr())
     {
         visit(ctx->expr());
-        std::cout << "    movq %rax, " << symbolTable[ctx->ID()->getText()].offset << "(%rbp) # visit decl\n";
+        std::cout << "    movq %rax, " << symbolTable[ctx->ID()->getText()].offset << "(%rbp)\n";
     }
     return 0;
 }
@@ -28,7 +28,7 @@ antlrcpp::Any CodeGenVisitor::visitAssign_stmt(ifccParser::Assign_stmtContext *c
 {
     visit(ctx->expr());
 
-    std::cout << "    movq %rax, " << symbolTable[ctx->ID()->getText()].offset << "(%rbp) # visit assign\n";
+    std::cout << "    movq %rax, " << symbolTable[ctx->ID()->getText()].offset << "(%rbp)\n";
 
     return 0;
 }
@@ -45,13 +45,74 @@ antlrcpp::Any CodeGenVisitor::visitReturn_stmt(ifccParser::Return_stmtContext *c
 
 antlrcpp::Any CodeGenVisitor::visitConst(ifccParser::ConstContext *ctx)
 {
-    // cehck type before printing
-    std::cout << "    movq $" << ctx->CONST()->getText() << ", %rax # visit int\n";
+    if (ctx->CONST()->getText() == "0")
+    {
+        std::cout << "    xor %rax, %rax\n";
+    }
+    else
+    std::cout << "    movq $" << ctx->CONST()->getText() << ", %rax\n";
     return 0;
 }
 
 antlrcpp::Any CodeGenVisitor::visitIdUse(ifccParser::IdUseContext *ctx)
 {
-    std::cout << "    movq " << symbolTable[ctx->ID()->getText()].offset << "(%rbp), %rax # visit idUse\n";
+    std::cout << "    movq " << symbolTable[ctx->ID()->getText()].offset << "(%rbp), %rax\n";
     return 0;
+}
+
+antlrcpp::Any CodeGenVisitor::visitNotExpr(ifccParser::NotExprContext *ctx) {
+    visit(ctx->primary());
+    std::cout << "    not %rax\n";
+    return 0;
+}
+
+antlrcpp::Any CodeGenVisitor::visitUnaryMinusExpr(ifccParser::UnaryMinusExprContext *ctx) {
+    visit(ctx->primary());
+    std::cout << "    neg %rax\n";
+    return 0;
+}
+
+antlrcpp::Any CodeGenVisitor::visitMulDivExpr(ifccParser::MulDivExprContext *ctx) {
+    visit(ctx->left());
+    std::cout << "    movq %rax, %rbx\n";
+    visit(ctx->right());
+    std::cout << "    movq %rbx, %rcx\n";
+    if (ctx->mOp()->STAR())
+    {
+        std::cout << "    imul %rcx, %rax\n";
+    }
+    else
+    {
+        std::cout << "    cqo\n";
+        std::cout << "    idiv %rcx\n";
+    }
+    return 0;
+}
+
+antlrcpp::Any CodeGenVisitor::visitAddSubExpr(ifccParser::AddSubExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitCompExpr(ifccParser::CompExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitEqExpr(ifccParser::EqExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitAndExpr(ifccParser::AndExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitXorExpr(ifccParser::XorExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitOrExpr(ifccParser::OrExprContext *ctx) {
+    
+}
+
+antlrcpp::Any CodeGenVisitor::visitParExpr(ifccParser::ParExprContext *ctx) {
+    
 }
--- compiler/CodeGenVisitor.h
+++ compiler/CodeGenVisitor.h
@@ -14,6 +14,16 @@ public:
     virtual antlrcpp::Any visitAssign_stmt(ifccParser::Assign_stmtContext *ctx) override;
     virtual antlrcpp::Any visitConst(ifccParser::ConstContext *ctx) override;
     virtual antlrcpp::Any visitIdUse(ifccParser::IdUseContext *ctx) override;
+    virtual antlrcpp::Any visitNotExpr(ifccParser::NotExprContext *ctx) override;
+    virtual antlrcpp::Any visitUnaryMinusExpr(ifccParser::UnaryMinusExprContext *ctx) override;
+    virtual antlrcpp::Any visitMulDivExpr(ifccParser::MulDivExprContext *ctx) override;
+    virtual antlrcpp::Any visitAddSubExpr(ifccParser::AddSubExprContext *ctx) override;
+    virtual antlrcpp::Any visitCompExpr(ifccParser::CompExprContext *ctx) override;
+    virtual antlrcpp::Any visitEqExpr(ifccParser::EqExprContext *ctx) override;
+    virtual antlrcpp::Any visitAndExpr(ifccParser::AndExprContext *ctx) override;
+    virtual antlrcpp::Any visitXorExpr(ifccParser::XorExprContext *ctx) override;
+    virtual antlrcpp::Any visitOrExpr(ifccParser::OrExprContext *ctx) override;
+    virtual antlrcpp::Any visitParExpr(ifccParser::ParExprContext *ctx) override;
 private:
     std::map<std::string, VarInfos> symbolTable;
-};
+};;

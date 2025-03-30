e0cc5911bc95314f4ccb2d9d16b92016a2b61e2b
Simon Perrigault <simonperrigault22@gmail.com>
2025-03-30 12:54:22 +0200

    4.17 support des double

---
+++ tests/testfiles/15_StaticChecks/15_7_mod_que_int.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 1, b = 5;
+    double c = 1.6;
+
+    int d = a % b;
+    int e = a % c;
+
+    return 0;
+}
---
+++ tests/testfiles/16_Double/16_11_sub.c
@@ -0,0 +1,7 @@
+int main() {
+    double x = 1.7, y = 5.6;
+
+    double z = y - x;
+
+    return z;
+}
